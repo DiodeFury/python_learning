73e7e589bcadeedad4c47d4383e2c562c1ab4514
Diode <854601623@qq.com>
2020-02-17 20:02:07 +0800

    1

--- class.py
+++ class.py
@@ -1,5 +1,5 @@
 class Point:
-    def __init__(self):
+    def __init__(self,x,y):
         self.x=x
         self.y=y
     def move(self):
@@ -16,5 +16,5 @@ class Point:
 # point1.draw()
 
 point = Point(10,20)
-point.x=12
+point.x=11
 print(point.x)
