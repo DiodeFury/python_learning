1c678162ad83dd289deaf7a0854ce39b09794eac
Diode <854601623@qq.com>
2020-02-18 18:20:03 +0800

    1

--- inheritance.py
+++ inheritance.py
@@ -5,12 +5,18 @@ class mammal:  #哺乳动物
 
 
 class dog(mammal):      #从mammal（父类）继承walk
-    pass                #python不喜欢空类，所以加一个pass
+    #pass                当类为空时，python不喜欢空类，所以加一个pass
+    def bark (self):
+        print("wangwangwang")
 
 
 class cat(mammal):
-    pass
+    #pass
+    def miao(self):
+        print("miaomiaomiao")
 
 
 dog1 = dog()
-dog1.walk()
+dog1.bark()
+cat2=cat()
+cat2.miao()
