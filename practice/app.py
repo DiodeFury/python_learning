ce523ac03f880a943a3a321d8ba2583d3c095223
Diode <854601623@qq.com>
2020-02-19 21:50:51 +0800

    1

--- practice/app.py
+++ practice/app.py
@@ -2,5 +2,12 @@ import openpyxl as xl
 wb = xl.load_workbook('C:/Users/AFH轰炸机/Desktop/python/pytest/practice/1.xlsx')
 sheet=wb['Sheet1']
 cell=sheet['a1']
-cell=sheet.cell(1,1)
-print(cell.value)
+cell=sheet.cell(1,1)#也可以这样表示
+print(sheet.max_row)
+for row in range(2,sheet.max_row+1):
+    cell = sheet.cell(row,3)
+    print(cell.value)
+    correct_cell_price = cell.value * 0.9
+    correct_cell = sheet.cell(row, 4)
+    correct_cell.value = correct_cell_price
+wb.save('2.xlsx')
