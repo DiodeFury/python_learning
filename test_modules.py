cbe49d1bb5483b4ad430a51a386a944c22ef3a61
Diode <854601623@qq.com>
2020-02-19 20:20:04 +0800

    1

--- test_modules.py
+++ test_modules.py
@@ -2,16 +2,16 @@
 # ecommerce.shiping.calc_shipping()
 # from ecommerce import shiping
 # shiping.calc_shipping()
-import random          #调用系统库
-for i in range(3):
-    print(random.randint(10,20))
-members = ['john', 'mosh', 'linda', 'marry']
-print(random.choice(members))
-class dice:
-    def roll(self):
-        first = random.randint(1, 6)
-        second = random.randint(1, 6)
-        return first,second
+# import random          #调用系统库
+# for i in range(3):
+#     print(random.randint(10,20))
+# members = ['john', 'mosh', 'linda', 'marry']
+# print(random.choice(members))
+# class dice:
+#     def roll(self):
+#         first = random.randint(1, 6)
+#         second = random.randint(1, 6)
+#         return first,second
 
 
 # dic = dice()
@@ -21,9 +21,9 @@ class dice:
 # path = Path()
 # for file in path .glob('*.py'):
 #     print(file)
-# import matplotlib.pyplot as plt
-# import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
 
-# x = np.linspace(0,20,100)   # 创建一个列表
-# plt.plot(x,np.sin(x))       # 对于每个点的sin值绘图
-# plt.show()                  # 显示
+x = np.linspace(0, 20, 100)  # 创建一个列表
+plt.plot(x, np.sin(x))  # 对于每个点的sin值绘图
+plt.show()  # 显示
